bfb383bbc9b07232290d1aedc7d188d8b25ba27e
sapoepsilon <47342870+sapoepsilon@users.noreply.github.com>
2021-02-22 22:26:07 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ismatulla Mansurov on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
